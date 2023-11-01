92803c91f93e045e621bfde857c08774e1884805
Smiran71 <142371779+Smiran71@users.noreply.github.com>
2023-11-01 16:43:17 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,24 +1,24 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database="chatbot";
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$database="chatbot";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password,$database);
+//$conn = mysqli_connect($servername, $username, $password,$database);
 
 // Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
+//if (!$conn) {
+  //die("Connection failed: " . mysqli_connect_error());
+//}
 //echo "Connected successfully";
 
 
-//  $con = mysqli_init();
-// mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-// mysqli_real_connect($con, "mysqlchat.mysql.database.azure.com", "sakshi","mayuri@2023", "chatbot", 3306, MYSQLI_CLIENT_SSL);
-// if (!$con) {
-//   die("Connection failed: " . mysqli_connect_error());
-// }
-// echo "Connected successfully";
-// ?> 
+  $conn = mysqli_init();
+ mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+ mysqli_real_connect($con, "mysqlchat.mysql.database.azure.com", "sakshi","mayuri@2023", "chatbot", 3306, MYSQLI_CLIENT_SSL);
+ if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+ }
+ echo "Connected successfully";
+ ?> 
